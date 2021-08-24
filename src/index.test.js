a692a9be651744ff8d57622cbd79883b204d726e
Elase Huang <elase.huang@outlook.com>
2021-08-24 20:38:23 +0800

    add function unique|flatten

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,13 @@
-import {typeOf} from "./index";
+import {flatten, typeOf, unique} from "./index";
 
-test('type of array', () => {
-  expect(typeOf([1,2])).toBe('Array');
+test('typeOf', () => {
+  expect(typeOf([1, 2])).toBe('Array');
+});
+
+test('unique', () => {
+  expect(unique([6, 3, 6, 8, 2, 1, 9, 4, 3, 6])).toEqual([6, 3, 8, 2, 1, 9, 4]);
+});
+
+test('flatten', () => {
+  expect(flatten([1, [2, 3], [4, [5, 6]]])).toEqual([1, 2, 3, 4, 5, 6]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,34 @@
+/**
+ * detect the datatype of obj
+ * @param obj
+ */
 export const typeOf = (obj: any) => {
-    const type = Object.prototype.toString.call(obj).toString()
-    return type.split(' ')[1].slice(0, -1)
+    return Object.prototype.toString.call(obj).toString()
+        .split(' ')[1].slice(0, -1)
+}
+/**
+ * unique the item of array
+ * @param arr
+ */
+export const unique = (arr: []) => {
+    return arr.filter((item, index, array) => {
+        return array.indexOf(item) === index
+    })
+}
+
+/**
+ * flat the array
+ * @param arr
+ */
+export const flatten = (arr: any[]): any[] => {
+    let res: any[] = new Array<any>()
+    for (let index = 0; index < arr.length; index++) {
+        const item = arr[index]
+        if (Array.isArray(item)) {
+            res.push(...flatten(item))
+        } else {
+            res.push(item)
+        }
+    }
+    return res
 }
